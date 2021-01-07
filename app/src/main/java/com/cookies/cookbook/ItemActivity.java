ce1f77c8d881c45a0ca699a59997075097f383dd
myusername <o.yangol@g.nsu.ru>
2021-01-08 02:34:16 +0700

    some fixes

--- app/src/main/java/com/cookies/cookbook/ItemActivity.java
+++ app/src/main/java/com/cookies/cookbook/ItemActivity.java
@@ -29,6 +29,7 @@ public class ItemActivity extends AppCompatActivity {
     private TextView name;
     private TableLayout ingredients;
     private ListView steps;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,44 +41,68 @@ public class ItemActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         int id = Integer.parseInt(intent.getStringExtra("r_id").split(":")[0]);
-        Call<Recipe> res = ApiClient.getInstance()
-                .getRecipeJsonApi()
-                .getById(id);
-        res.enqueue(new Callback<Recipe>() {
-            @Override
-            public void onResponse(Call<Recipe> call, Response<Recipe> response) {
-                if (!response.isSuccessful()) {
-                    categoryName.setText(response.code());
-                }
-
-                Recipe recipe = response.body();
-                String categories = "";
-                for (Category category: recipe.getCategories()) {
-                    categories += category.getName();
-                    categories += " * ";
-                }
-                categoryName.setText(categories);
-                name.setText(recipe.getName());
-                for (Ingredient ingredient: recipe.getIngredients()) {
-                    TableRow row = new TableRow(ItemActivity.this);
-                    TextView ing_name = new TextView(ItemActivity.this);
-                    TextView ing_amount = new TextView(ItemActivity.this);
-                    ing_name.setText(ingredient.getProduct().getName());
-                    ing_amount.setText(ingredient.getAmount());
-                }
-                ArrayList<String> steps_s = new ArrayList<>();
-                for (Step step: recipe.getSteps()){
-                    steps_s.add(step.getId() + " : " + step.getDescription());
-                }
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(ItemActivity.this, android.R.layout.simple_list_item_2, steps_s);
-                steps.setAdapter(adapter);
-            }
-
-            @Override
-            public void onFailure(Call<Recipe> call, Throwable t) {
-                categoryName.setText(t.getMessage());
-            }
-        });
-
+        getRecipe(id);
     }
+
+    private void getRecipe(int id) {
+        ApiClient.getInstance()
+                .getRecipeJsonApi()
+                .getById(id)
+                .enqueue(new Callback<Recipe>() {
+                    @Override
+                    public void onResponse(Call<Recipe> call, Response<Recipe> response) {
+                        if (!response.isSuccessful()) {
+                            categoryName.setText(response.code());
+                        }
+
+                        Recipe recipe = response.body();
+                        setRecipeInfo(recipe);
+                    }
+
+                    @Override
+                    public void onFailure(Call<Recipe> call, Throwable t) {
+                        categoryName.setText(t.getMessage());
+                    }
+                });
+    }
+
+    private void setRecipeInfo(Recipe recipe) {
+        setCategoryName(recipe.getCategories());
+        name.setText(recipe.getName());
+        setIngredients(recipe.getIngredients());
+        setSteps(recipe.getSteps());
+    }
+
+    private void setCategoryName(List<Category> recipeCategories) {
+        StringBuilder categories = new StringBuilder();
+        for (Category category : recipeCategories) {
+            categories.append(category.getName());
+            categories.append(" * ");
+        }
+        categoryName.setText(categories.toString());
+    }
+
+    private void setIngredients(List<Ingredient> recipeIngredients) {
+        for (Ingredient ingredient : recipeIngredients) {
+            TableRow row = new TableRow(ItemActivity.this);
+            TextView ing_name = new TextView(ItemActivity.this);
+            TextView ing_amount = new TextView(ItemActivity.this);
+            ing_name.setText(ingredient.getProduct().getName());
+            ing_amount.setText(String.valueOf(ingredient.getAmount()));
+            row.addView(ing_name, 0);
+            row.addView(ing_amount, 1);
+            ingredients.addView(row);
+        }
+    }
+
+    private void setSteps(List<Step> recipeSteps) {
+        ArrayList<String> stepDescriptions = new ArrayList<>();
+        for (Step step : recipeSteps) {
+            stepDescriptions.add(step.getStepNumber() + " : " + step.getDescription());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ItemActivity.this,
+                android.R.layout.simple_list_item_1, stepDescriptions);
+        steps.setAdapter(adapter);
+    }
+
 }
--- app/src/main/java/com/cookies/cookbook/MainActivity.java
+++ app/src/main/java/com/cookies/cookbook/MainActivity.java
@@ -12,11 +12,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.cookies.cookbook.api.model.BaseRecipe;
-import com.cookies.cookbook.api.model.Category;
-import com.cookies.cookbook.api.model.Recipe;
 import com.cookies.cookbook.network.ApiClient;
 
 import java.util.ArrayList;
@@ -26,49 +23,53 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private ListView listView;
+    private final ArrayList<String> recipesInfo = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //textView = findViewById(R.id.result);
+        textView = findViewById(R.id.result);
         listView = findViewById(R.id.lvRecipes);
-        Call<List<BaseRecipe>> res = ApiClient.getInstance()
+
+        getRecipes();
+        setRecipeList();
+    }
+
+    private void getRecipes() {
+        ApiClient.getInstance()
                 .getRecipeJsonApi()
-                .getAll();
-        ArrayList<String> resi = new ArrayList<>();
-        res.enqueue(new Callback<List<BaseRecipe>>() {
-            @Override
-            public void onResponse(Call<List<BaseRecipe>> call, Response<List<BaseRecipe>> response) {
-                if (!response.isSuccessful()) {
-                    Toast toast = Toast.makeText(getApplicationContext(),
-                            Integer.toString(response.code()),
-                            Toast.LENGTH_SHORT);
-                    toast.show();
-                }
+                .getAll()
+                .enqueue(new Callback<List<BaseRecipe>>() {
+                    @Override
+                    public void onResponse(Call<List<BaseRecipe>> call, Response<List<BaseRecipe>> response) {
+                        if (!response.isSuccessful()) {
+                            textView.setText(response.code());
+                        }
 
-                List<BaseRecipe> recipes = response.body();
+                        List<BaseRecipe> recipes = response.body();
+                        for (BaseRecipe recipe : recipes) {
+                            textView.append(" name  " + recipe.getName());
+                            recipesInfo.add(recipe.getId() + ":" + recipe.getName());
+                        }
+                    }
 
-                for (BaseRecipe recipe : recipes) {
-                    resi.add(recipe.getId() + ":" + recipe.getName());
-                }
-            }
+                    @Override
+                    public void onFailure(Call<List<BaseRecipe>> call, Throwable t) {
+                        textView.setText(t.getMessage());
+                    }
+                });
+    }
 
-            @Override
-            public void onFailure(Call<List<BaseRecipe>> call, Throwable t) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        t.getMessage(),
-                        Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, resi);
+    private void setRecipeList() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, recipesInfo);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(MainActivity.this, ItemActivity.class);
-                intent.putExtra("r_id", ((TextView)view).getText().toString());
+                intent.putExtra("r_id", ((TextView) view).getText().toString());
                 startActivity(intent);
             }
         });
